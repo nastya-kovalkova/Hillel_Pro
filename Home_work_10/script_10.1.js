8fe77821c003d0e749d6eae375ccd132fd6739f7
nastya-kovalkova <nastyakovalkova333@gmail.com>
2024-12-09 08:40:22 +0200

    HW-10.1(1)

--- Home_work_10/script_10.1.js
+++ Home_work_10/script_10.1.js
@@ -1,15 +1,42 @@
-const user = {
-  firstName: "Anastasiia",
-  lastName: "Kovalkova",
-  age: 30,
-  location: "Poznan",
-  profession: "Frontend Developer",
+// const user = {
+//   firstName: "Anastasiia",
+//   lastName: "Kovalkova",
+//   age: 30,
+//   location: "Poznan",
+//   profession: "Frontend Developer",
+//
+//   showInfo: function () {
+//     const info = `Full name: ${this.firstName} ${this.lastName}\nAge: ${this.age}\nLocation: ${this.location}\nProfession: ${this.profession}`;
+//     console.log(info);
+//     return info;
+//   },
+// };
+//
+// user.showInfo();
 
-  showInfo: function () {
-    const info = `Full name: ${this.firstName} ${this.lastName}\nAge: ${this.age}\nLocation: ${this.location}\nProfession: ${this.profession}`;
-    console.log(info);
-    return info;
-  },
+function User(name, surname, age, location, profession) {
+  this.name = name;
+  this.surname = surname;
+  this.age = age;
+  this.location = location;
+  this.profession = profession;
+}
+
+User.prototype.getInfo = () => {
+  return {
+    name: this.name,
+    surname: this.surname,
+    age: this.age,
+    location: this.location,
+    profession: this.profession,
+  };
 };
 
-user.showInfo();
+const user = new User(
+  "Anastasiia",
+  "Kovalkova",
+  30,
+  "Poznan",
+  "Frontend developer",
+);
+console.log(user);
