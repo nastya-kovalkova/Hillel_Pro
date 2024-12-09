a091dee907a1f81a181abc177701fcb074d3fc2e
nastya-kovalkova <nastyakovalkova333@gmail.com>
2024-12-09 09:04:44 +0200

    HW-10.1(2)

--- Home_work_10/script_10.2.js
+++ Home_work_10/script_10.2.js
@@ -1,1 +1,15 @@
 const numbers = [1, 0, 10, 5, 2, 4, 7, 11, 8, 9];
+
+function getEvenNumbers(array) {
+  let evenNumbers = [];
+  array.forEach((number) => {
+    if (number % 2 === 0 && number !== 0) {
+      evenNumbers.push(number);
+    }
+  });
+
+  return evenNumbers;
+}
+
+const even = getEvenNumbers(numbers);
+console.log(even); // [10, 2, 4, 8]
